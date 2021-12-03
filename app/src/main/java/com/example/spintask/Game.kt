931fa9ae083bc0ae95a6f38242219c606ce427ab
y62 <y62@outlook.dk>
2021-12-03 22:19:05 +0100

    almost done

---
+++ app/src/main/java/com/example/spintask/Game.kt
@@ -0,0 +1,135 @@
+package com.example.spintask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.spintask.databinding.FragmentGameBinding
+
+class Game : Fragment(R.layout.fragment_game) {
+    private var _binding : FragmentGameBinding? = null
+    private val binding get () = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+         var point: Int = 1000
+         var life: Int = 5
+         val spinResult = arrayOf("Bankrupt", "1000 Extra!", "Miss Turn", "Extra Turn!" )
+
+        binding.spinButton.setOnClickListener {
+            val numbers = (0..3).random()
+            if (spinResult[numbers] == spinResult[0]) {
+                point = 0
+            }
+            if (spinResult[numbers] == spinResult[1]) {
+                point += 1000
+            }
+            if (spinResult[numbers] == spinResult[2]) {
+                life -= 1
+            }
+            if (spinResult[numbers] == spinResult[3]) {
+                life += 1
+            }
+       /*     if (life <= 0) {
+                val toast = Toast.makeText(this,"GAME OVER!", Toast.LENGTH_SHORT)
+                toast.show()
+            } */
+            binding.spinResultText.text = spinResult[numbers]
+            binding.lives.text = life.toString()
+            binding.points.text = point.toString()
+        }
+
+        //_______________________________________________________________________________
+        val cars = arrayOf("bmw", "mercedes", "toyota", "ferrari", "peugeot", "nissan", "tesla", "ford" )
+        val countries = arrayOf("japan", "china", "canada", "brazil", "venezuela", "egypt", "australia", "denmark" )
+        val cities = arrayOf("copenhagen", "sydney", "moscow", "delhi", "kyoto", "phoenix", "berlin", "paris", "bucharest" )
+        val animals = arrayOf("elephant", "leopard", "cheetah", "crocodile", "horse", "donkey", "rabbit", "squirrel" )
+        val brands = arrayOf("adidas", "gucci", "pringles", "pepsi", "microsoft", "visa", "nivea", "lacoste", "starbucks" )
+        val celebrities = arrayOf("madonna", "trump", "drake", "rihanna", "eminem", "putin", "undertaker" )
+        val categories = arrayOf(cars, countries, cities, animals, brands, celebrities)
+        val categoryNames = arrayOf("cars", "countries", "cities", "animals", "brands", "celebrities")
+        val categoryRandomizer = (0..5).random()
+        val randomCategory = categories[categoryRandomizer]
+        val randomCategoryWord = randomCategory[(0..randomCategory.lastIndex).random()]
+        val questionMarks = mutableListOf<String>()
+        val letters = mutableListOf<String>()
+        var word: String = ""
+        var tempWord: String = ""
+        var questionWord: String = ""
+        var tempQuestionWord: String = ""
+        var questionMarkSigns: String = ""
+        println("Category " +  categoryNames[categoryRandomizer])
+        binding.category.text = categoryNames[categoryRandomizer].toString()
+
+        for (i in randomCategoryWord.indices) {
+            questionMarks.add("?")
+            questionMarkSigns += "?"
+        }
+
+        for (i in randomCategoryWord.indices) {
+            letters.add(randomCategoryWord[i].toString())
+        }
+
+        for (i in letters.indices) {
+            tempWord = letters[i].toString()
+            word += tempWord
+        }
+
+        binding.guessResult.text = questionMarkSigns.toString()
+
+        binding.enter.setOnClickListener{
+            tempQuestionWord = ""
+            for (i in letters.indices) {
+                if (letters[i] == binding.guessTheWord.text.toString()) {
+                    questionMarks[i] = letters[i]
+                }
+            }
+
+            if (!letters.contains(binding.guessTheWord.text.toString())) {
+                life--
+            }
+
+            questionWord = ""
+            for (i in questionMarks.indices) {
+                tempQuestionWord = questionMarks[i].toString()
+                questionWord += tempQuestionWord
+            }
+            binding.guessResult.text = questionWord.toString()
+            binding.lives.text = life.toString()
+
+          /*  if (questionWord == word) {
+                val toast = Toast.makeText(this,"YOU WIN!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+            if (life <= 0) {
+                val toast = Toast.makeText(this,"GAME OVER!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+*/
+            //  binding.guessResult.text = binding.guessTheWord.text.toString()
+            // binding.guessResult.text = cars[1].toString()
+        }
+
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/example/spintask/MainActivity.kt
+++ app/src/main/java/com/example/spintask/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.spintask
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.spintask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,104 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.restart.setOnClickListener {
-            val intent: Intent = intent
-            finish()
-            startActivity(intent);
-        }
-        binding.spinButton.setOnClickListener {
-            val numbers = (0..3).random()
-            if (spinResult[numbers] == spinResult[0]) {
-                point = 0
-            }
-            if (spinResult[numbers] == spinResult[1]) {
-                point += 1000
-            }
-            if (spinResult[numbers] == spinResult[2]) {
-                life -= 1
-            }
-            if (spinResult[numbers] == spinResult[3]) {
-                life += 1
-            }
-            if (life <= 0) {
-                val toast = Toast.makeText(this,"GAME OVER!", Toast.LENGTH_SHORT)
-                toast.show()
-            }
-            binding.spinResultText.text = spinResult[numbers]
-            binding.lives.text = life.toString()
-            binding.points.text = point.toString()
-        }
 
-        //_______________________________________________________________________________
-        val cars = arrayOf("bmw", "mercedes", "toyota", "ferrari", "peugeot", "nissan", "tesla", "ford" )
-        val countries = arrayOf("japan", "china", "canada", "brazil", "venezuela", "egypt", "australia", "denmark" )
-        val cities = arrayOf("copenhagen", "sydney", "moscow", "delhi", "kyoto", "phoenix", "berlin", "paris", "bucharest" )
-        val animals = arrayOf("elephant", "leopard", "cheetah", "crocodile", "horse", "donkey", "rabbit", "squirrel" )
-        val brands = arrayOf("adidas", "gucci", "pringles", "pepsi", "microsoft", "visa", "nivea", "lacoste", "starbucks" )
-        val celebrities = arrayOf("madonna", "trump", "drake", "rihanna", "eminem", "putin", "undertaker" )
-        val categories = arrayOf(cars, countries, cities, animals, brands, celebrities)
-        val categoryNames = arrayOf("cars", "countries", "cities", "animals", "brands", "celebrities")
-        val categoryRandomizer = (0..5).random()
-        val randomCategory = categories[categoryRandomizer]
-        val randomCategoryWord = randomCategory[(0..randomCategory.lastIndex).random()]
-        val questionMarks = mutableListOf<String>()
-        val letters = mutableListOf<String>()
-        var word: String = ""
-        var tempWord: String = ""
-        var questionWord: String = ""
-        var tempQuestionWord: String = ""
-        var questionMarkSigns: String = ""
-println("Category " +  categoryNames[categoryRandomizer])
-        binding.category.text = categoryNames[categoryRandomizer].toString()
-
-        for (i in randomCategoryWord.indices) {
-            questionMarks.add("?")
-            questionMarkSigns += "?"
-        }
-
-        for (i in randomCategoryWord.indices) {
-            letters.add(randomCategoryWord[i].toString())
-        }
-
-        for (i in letters.indices) {
-            tempWord = letters[i].toString()
-            word += tempWord
-        }
-
-        binding.guessResult.text = questionMarkSigns.toString()
-
-        binding.enter.setOnClickListener{
-            tempQuestionWord = ""
-            for (i in letters.indices) {
-                if (letters[i] == binding.guessTheWord.text.toString()) {
-                    questionMarks[i] = letters[i]
-                }
-            }
-
-            if (!letters.contains(binding.guessTheWord.text.toString())) {
-                life--
-            }
-
-            questionWord = ""
-            for (i in questionMarks.indices) {
-                tempQuestionWord = questionMarks[i].toString()
-                questionWord += tempQuestionWord
-            }
-            binding.guessResult.text = questionWord.toString()
-            binding.lives.text = life.toString()
-
-            if (questionWord == word) {
-                val toast = Toast.makeText(this,"YOU WIN!", Toast.LENGTH_SHORT)
-                toast.show()
-            }
-
-            if (life <= 0) {
-                val toast = Toast.makeText(this,"GAME OVER!", Toast.LENGTH_SHORT)
-                toast.show()
-            }
-
-             //  binding.guessResult.text = binding.guessTheWord.text.toString()
-            // binding.guessResult.text = cars[1].toString()
+        fun replaceFragment(fragment: Fragment) {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment, fragment) //here
+            fragmentTransaction.commit()
         }
 
         binding.restart.setOnClickListener {
@@ -122,6 +30,16 @@ println("Category " +  categoryNames[categoryRandomizer])
             finish()
             startActivity(intent);
         }
+
+        binding.playBtn.setOnClickListener {
+            replaceFragment(Game())
+        }
+/*
+        binding.button3.setOnClickListener {
+            replaceFragment(GameRules())
+        }
+*/
+
     }
 
 
